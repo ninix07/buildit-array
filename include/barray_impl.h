fa42a90365b41c9b4b51597f474821bfc7b72c97
Nigam Niraula <nigam21nir@gmail.com>
2024-10-25 23:12:00 +0530

    TODO 5

--- include/barray_impl.h
+++ include/barray_impl.h
@@ -8,292 +8,337 @@
 #include <cmath>
 #include <cstdio>
 
-namespace barray {
+namespace barray
+{
 
-// Enum to keep track of where the code is executing
-// and where the data is stored
-enum current_device_t {
-  DEVICE_HOST,
-  DEVICE_GPU,
-};
+  // Enum to keep track of where the code is executing
+  // and where the data is stored
+  enum current_device_t
+  {
+    DEVICE_HOST,
+    DEVICE_GPU,
+  };
 
-// Global variable to keep track of current execution device
-extern current_device_t current_device;
+  // Global variable to keep track of current execution device
+  extern current_device_t current_device;
 
-// Helper function to check if two arrays are compatible for pointwise
-// operations and return the resultant size
-std::vector<int> match_expr_sizes(std::vector<int>, std::vector<int>);
+  // Helper function to check if two arrays are compatible for pointwise
+  // operations and return the resultant size
+  std::vector<int> match_expr_sizes(std::vector<int>, std::vector<int>);
 
-// Base class for all expressions that can appear on the RHS of a =
-template <typename T> struct barray_expr {
-  virtual const builder::dyn_var<T>
-  get_value_at(std::vector<builder::dyn_var<int> *> indices) const {
-    return 0;
-  }
-  virtual const std::vector<int> get_expr_size(void) const { return {}; }
-};
+  // Base class for all expressions that can appear on the RHS of a =
+  template <typename T>
+  struct barray_expr
+  {
+    virtual const builder::dyn_var<T>
+    get_value_at(std::vector<builder::dyn_var<int> *> indices) const
+    {
+      return 0;
+    }
+    virtual const std::vector<int> get_expr_size(void) const { return {}; }
+  };
 
-template <typename T> struct barray_expr_const;
-template <typename T> struct barray_expr_array;
+  template <typename T>
+  struct barray_expr_const;
+  template <typename T>
+  struct barray_expr_array;
 
-// Basic barray class. This class provides the programming interface
-// for the barray language
-template <typename T> struct barray {
+  // Basic barray class. This class provides the programming interface
+  // for the barray language
+  template <typename T>
+  struct barray
+  {
 
-  // Members to keep track of data and sizes
-  builder::dyn_var<T *> m_arr = 0;
+    // Members to keep track of data and sizes
+    builder::dyn_var<T *> m_arr = 0;
 
-  // GPU related buffers
-  builder::dyn_var<T *> m_device_arr = 0;
-  builder::static_var<int> device_allocated = 0;
-  builder::static_var<int> current_storage = DEVICE_HOST;
-  std::vector<int> m_sizes;
+    // GPU related buffers
+    builder::dyn_var<T *> m_device_arr = 0;
+    builder::static_var<int> device_allocated = 0;
+    builder::static_var<int> current_storage = DEVICE_HOST;
+    std::vector<int> m_sizes;
 
-  // Optimization related fields
-  builder::static_var<int> is_constant = false;
-  builder::static_var<T> constant_val = 0;
+    // Optimization related fields
+    builder::static_var<int> is_constant = false;
+    builder::static_var<T> constant_val = 0;
 
-  // Basic constructor to initialize barray
-  // from underlying buffer and sizes
-  barray(builder::dyn_var<T *> arr, std::vector<int> sizes)
-      : m_arr(arr), m_sizes(sizes) {}
+    // Basic constructor to initialize barray
+    // from underlying buffer and sizes
+    barray(builder::dyn_var<T *> arr, std::vector<int> sizes)
+        : m_arr(arr), m_sizes(sizes) {}
 
-  // Constructor to allocate a new barray with provided size
-  barray(std::vector<int> sizes) { allocate(sizes); }
+    // Constructor to allocate a new barray with provided size
+    barray(std::vector<int> sizes) { allocate(sizes); }
 
-  // Function to compute flat array size
-  int get_total_size() {
-    int total_size = 1;
-    for (auto size : m_sizes)
-      total_size *= size;
-    return total_size;
-  }
+    // Function to compute flat array size
+    int get_total_size()
+    {
+      int total_size = 1;
+      for (auto size : m_sizes)
+        total_size *= size;
+      return total_size;
+    }
 
-  // Manual allocate function to allocate data with given dimensions
-  void allocate(std::vector<int> sizes) {
-    m_sizes = sizes;
-    m_arr = runtime::malloc((int)sizeof(T) * get_total_size());
-  }
+    // Manual allocate function to allocate data with given dimensions
+    void allocate(std::vector<int> sizes)
+    {
+      m_sizes = sizes;
+      m_arr = runtime::malloc((int)sizeof(T) * get_total_size());
+    }
 
-  // Function to allocate memory on the GPU
-  void allocate_device(std::vector<int> sizes) {
-    if (device_allocated)
-      return;
-    m_device_arr = runtime::cuda_malloc((int)sizeof(T) * get_total_size());
-    device_allocated = 1;
-  }
+    // Function to allocate memory on the GPU
+    void allocate_device(std::vector<int> sizes)
+    {
+      if (device_allocated)
+        return;
+      m_device_arr = runtime::cuda_malloc((int)sizeof(T) * get_total_size());
+      device_allocated = 1;
+    }
 
-  // Function to transfer the array to the GPU
-  void to_device() {
-    allocate_device(m_sizes);
-    if (current_storage == DEVICE_GPU)
-      return;
-    runtime::cuda_memcpy_to_device(m_device_arr, m_arr,
+    // Function to transfer the array to the GPU
+    void to_device()
+    {
+      allocate_device(m_sizes);
+      if (current_storage == DEVICE_GPU)
+        return;
+      runtime::cuda_memcpy_to_device(m_device_arr, m_arr,
+                                     (int)sizeof(T) * get_total_size());
+    }
+
+    // Function to transfer the array back to the HOST
+    void to_host()
+    {
+      if (current_storage == DEVICE_HOST)
+        return;
+      runtime::cuda_memcpy_to_host(m_arr, m_device_arr,
                                    (int)sizeof(T) * get_total_size());
-  }
-
-  // Function to transfer the array back to the HOST
-  void to_host() {
-    if (current_storage == DEVICE_HOST)
-      return;
-    runtime::cuda_memcpy_to_host(m_arr, m_device_arr,
-                                 (int)sizeof(T) * get_total_size());
-  }
-
-  // Delete the default implementations of the operators
-  // and copy constructors
-  barray(const barray &other) = delete;
-
-  // Helper function to induce loop nests and compute values
-  void induce_loop_at(std::vector<builder::dyn_var<int> *> indices,
-                      const barray_expr<T> &rhs) {
-    unsigned int index = indices.size();
-
-    if (index == m_sizes.size()) {
-      if (current_device == DEVICE_HOST)
-        m_arr[compute_flat_index(indices)] = rhs.get_value_at(indices);
-      else
-        m_device_arr[compute_flat_index(indices)] = rhs.get_value_at(indices);
-      return;
     }
 
-    builder::dyn_var<int> i = 0;
-    indices.push_back(i.addr());
-    // TODO 5: Map loop nests to GPU if current device is GPU
+    // Delete the default implementations of the operators
+    // and copy constructors
+    barray(const barray &other) = delete;
 
-    // TODO 2: Recursively induce loops for all dimensions
+    // Helper function to induce loop nests and compute values
+    void induce_loop_at(std::vector<builder::dyn_var<int> *> indices,
+                        const barray_expr<T> &rhs)
+    {
+      unsigned int index = indices.size();
 
-    for (; i < m_sizes[index]; i++)
-      induce_loop_at(indices, rhs);
-  }
+      if (index == m_sizes.size())
+      {
+        if (current_device == DEVICE_HOST)
+          m_arr[compute_flat_index(indices)] = rhs.get_value_at(indices);
+        else
+          m_device_arr[compute_flat_index(indices)] = rhs.get_value_at(indices);
+        return;
+      }
 
-  // Assignment operator overloads
-  void operator=(const barray_expr<T> &rhs) {
-    match_expr_sizes(m_sizes, rhs.get_expr_size());
-    induce_loop_at({}, rhs);
-    is_constant = false;
-    current_storage = current_device;
-  }
+      // TODO 5: Map loop nests to GPU if current device is GPU
+      if (current_device == DEVICE_GPU && index == 0)
+      {
+        builder::annotate(CUDA_KERNEL);
+      }
+      builder::dyn_var<int> i = 0;
+      indices.push_back(i.addr());
+      // TODO 2: Recursively induce loops for all dimensions
 
-  void operator=(const T &value) {
-    int total_size = get_total_size();
-
-    // TODO 1: Initialize all elements of the array to value
-
-    /*for (builder::dyn_var<int> i = 0; i < total_size; i++) {*/
-    /*  m_arr[i] = value;*/
-    /*}*/
-    // TODO 4: Optimize arrays initialized to constants
-    is_constant = true;
-    constant_val = value;
-    current_storage = DEVICE_HOST;
-  }
-
-  void operator=(const barray &other) { *this = barray_expr_array<T>(other); }
-
-  // Function to convert multi-dimensional index into a single index
-  // based on the sizes
-  builder::dyn_var<int>
-  compute_flat_index(std::vector<builder::dyn_var<int> *> indices,
-                     unsigned int index = -1) const {
-    if (index == -1)
-      index = m_sizes.size() - 1;
-    if (index == 0)
-      return *(indices[index]);
-    return *(indices[index]) +
-           (int)m_sizes[index] * compute_flat_index(indices, index - 1);
-  }
-
-  // Function to write the constant value to the array if
-  // it is optimized
-  void finalize() {
-    if (!is_constant)
-      return;
-    *this = barray_expr_const<T>((T)constant_val);
-  }
-
-  void destroy() { runtime::free(m_arr); }
-};
-
-// RHS class for constants in the generated code
-template <typename T> struct barray_expr_const : public barray_expr<T> {
-  T m_value;
-  barray_expr_const(const T &value) : m_value(value) {}
-
-  const builder::dyn_var<T>
-  get_value_at(std::vector<builder::dyn_var<int> *> indices) const {
-    // Always return the same constant value
-    return m_value;
-  }
-
-  const std::vector<int> get_expr_size(void) const { return {}; }
-};
-
-// RHS class for holding array operands
-template <typename T> struct barray_expr_array : public barray_expr<T> {
-  const struct barray<T> &m_array;
-  barray_expr_array(const struct barray<T> &array) : m_array(array) {}
-
-  const builder::dyn_var<T>
-  get_value_at(std::vector<builder::dyn_var<int> *> indices) const {
-    // TODO 6: Make sure the array is on the GPU if requested from GPU
-
-    // TODO 4.2: Optimize arrays initialized to constants
-    if (m_array.is_constant) {
-      return m_array.constant_val;
-    } else {
-      if (current_device == DEVICE_HOST)
-        return const_cast<builder::dyn_var<T *> &>(
-            m_array.m_arr)[m_array.compute_flat_index(indices)];
-      else
-        return const_cast<builder::dyn_var<T *> &>(
-            m_array.m_device_arr)[m_array.compute_flat_index(indices)];
+      for (; i < m_sizes[index]; i++)
+        induce_loop_at(indices, rhs);
     }
-  }
 
-  const std::vector<int> get_expr_size(void) const { return m_array.m_sizes; }
-};
+    // Assignment operator overloads
+    void operator=(const barray_expr<T> &rhs)
+    {
+      match_expr_sizes(m_sizes, rhs.get_expr_size());
+      induce_loop_at({}, rhs);
+      is_constant = false;
+      current_storage = current_device;
+    }
 
-// Non leaf RHS expr classes
+    void operator=(const T &value)
+    {
+      int total_size = get_total_size();
 
-// Class for storing addition of two expressions
-template <typename T> struct barray_expr_add : public barray_expr<T> {
-  const struct barray_expr<T> &expr1;
-  const struct barray_expr<T> &expr2;
+      // TODO 1: Initialize all elements of the array to value
 
-  std::vector<int> computed_sizes;
+      for (builder::dyn_var<int> i = 0; i < total_size; i++)
+      {
+        m_arr[i] = value;
+      }
+      // TODO 4: Optimize arrays initialized to constants
+      is_constant = true;
+      constant_val = value;
+      current_storage = DEVICE_HOST;
+    }
 
-  barray_expr_add(const struct barray_expr<T> &expr1,
-                  const struct barray_expr<T> &expr2)
-      : expr1(expr1), expr2(expr2) {
-    computed_sizes =
-        match_expr_sizes(expr1.get_expr_size(), expr2.get_expr_size());
-  }
+    void operator=(const barray &other) { *this = barray_expr_array<T>(other); }
 
-  const std::vector<int> get_expr_size(void) const { return computed_sizes; }
+    // Function to convert multi-dimensional index into a single index
+    // based on the sizes
+    builder::dyn_var<int>
+    compute_flat_index(std::vector<builder::dyn_var<int> *> indices,
+                       unsigned int index = -1) const
+    {
+      if (index == -1)
+        index = m_sizes.size() - 1;
+      if (index == 0)
+        return *(indices[index]);
+      return *(indices[index]) +
+             (int)m_sizes[index] * compute_flat_index(indices, index - 1);
+    }
 
-  const builder::dyn_var<T>
-  get_value_at(std::vector<builder::dyn_var<int> *> indices) const {
-    return expr1.get_value_at(indices) + expr2.get_value_at(indices);
-  }
-};
+    // Function to write the constant value to the array if
+    // it is optimized
+    void finalize()
+    {
+      if (!is_constant)
+        return;
+      *this = barray_expr_const<T>((T)constant_val);
+    }
 
-// Class for pointwise multiplication of two expressions
-template <typename T> struct barray_expr_mul : public barray_expr<T> {
-  const struct barray_expr<T> &expr1;
-  const struct barray_expr<T> &expr2;
+    void destroy() { runtime::free(m_arr); }
+  };
 
-  std::vector<int> computed_sizes;
+  // RHS class for constants in the generated code
+  template <typename T>
+  struct barray_expr_const : public barray_expr<T>
+  {
+    T m_value;
+    barray_expr_const(const T &value) : m_value(value) {}
 
-  barray_expr_mul(const struct barray_expr<T> &expr1,
-                  const struct barray_expr<T> &expr2)
-      : expr1(expr1), expr2(expr2) {
-    computed_sizes =
-        match_expr_sizes(expr1.get_expr_size(), expr2.get_expr_size());
-  }
+    const builder::dyn_var<T>
+    get_value_at(std::vector<builder::dyn_var<int> *> indices) const
+    {
+      // Always return the same constant value
+      return m_value;
+    }
 
-  const std::vector<int> get_expr_size(void) const { return computed_sizes; }
+    const std::vector<int> get_expr_size(void) const { return {}; }
+  };
 
-  const builder::dyn_var<T>
-  get_value_at(std::vector<builder::dyn_var<int> *> indices) const {
-    return expr1.get_value_at(indices) * expr2.get_value_at(indices);
-  }
-};
+  // RHS class for holding array operands
+  template <typename T>
+  struct barray_expr_array : public barray_expr<T>
+  {
+    const struct barray<T> &m_array;
+    barray_expr_array(const struct barray<T> &array) : m_array(array) {}
 
-// Class for cross product of two expressions
-template <typename T> struct barray_expr_cross : public barray_expr<T> {
-  const struct barray_expr<T> &expr1;
-  const struct barray_expr<T> &expr2;
+    const builder::dyn_var<T>
+    get_value_at(std::vector<builder::dyn_var<int> *> indices) const
+    {
+      // TODO 6: Make sure the array is on the GPU if requested from GPU
 
-  std::vector<int> computed_sizes;
+      // TODO 4.2: Optimize arrays initialized to constants
+      if (m_array.is_constant)
+      {
+        return m_array.constant_val;
+      }
+      else
+      {
+        if (current_device == DEVICE_HOST)
+          return const_cast<builder::dyn_var<T *> &>(
+              m_array.m_arr)[m_array.compute_flat_index(indices)];
+        else
+          return const_cast<builder::dyn_var<T *> &>(
+              m_array.m_device_arr)[m_array.compute_flat_index(indices)];
+      }
+    }
 
-  barray_expr_cross(const struct barray_expr<T> &expr1,
+    const std::vector<int> get_expr_size(void) const { return m_array.m_sizes; }
+  };
+
+  // Non leaf RHS expr classes
+
+  // Class for storing addition of two expressions
+  template <typename T>
+  struct barray_expr_add : public barray_expr<T>
+  {
+    const struct barray_expr<T> &expr1;
+    const struct barray_expr<T> &expr2;
+
+    std::vector<int> computed_sizes;
+
+    barray_expr_add(const struct barray_expr<T> &expr1,
                     const struct barray_expr<T> &expr2)
-      : expr1(expr1), expr2(expr2) {
-    std::vector<int> size1 = expr1.get_expr_size();
-    std::vector<int> size2 = expr2.get_expr_size();
-    assert(size1.size() == 2 && size2.size() == 2 &&
-           "Cross can only be performed on 2 dimensional arrays");
-    assert(size1[1] == size2[0] && "Dimensions should match for cross product");
-    computed_sizes.push_back(size1[0]);
-    computed_sizes.push_back(size2[1]);
-  }
+        : expr1(expr1), expr2(expr2)
+    {
+      computed_sizes =
+          match_expr_sizes(expr1.get_expr_size(), expr2.get_expr_size());
+    }
 
-  const std::vector<int> get_expr_size(void) const { return computed_sizes; }
+    const std::vector<int> get_expr_size(void) const { return computed_sizes; }
 
-  const builder::dyn_var<T>
-  get_value_at(std::vector<builder::dyn_var<int> *> indices) const {
-    builder::dyn_var<T> sum = 0;
-    builder::dyn_var<int> i = 0;
-    std::vector<builder::dyn_var<int> *> indices1 = {indices[0], i.addr()};
-    std::vector<builder::dyn_var<int> *> indices2 = {i.addr(), indices[1]};
-    int len = expr1.get_expr_size()[1];
+    const builder::dyn_var<T>
+    get_value_at(std::vector<builder::dyn_var<int> *> indices) const
+    {
+      return expr1.get_value_at(indices) + expr2.get_value_at(indices);
+    }
+  };
 
-    // TODO: Implement cross product operator
+  // Class for pointwise multiplication of two expressions
+  template <typename T>
+  struct barray_expr_mul : public barray_expr<T>
+  {
+    const struct barray_expr<T> &expr1;
+    const struct barray_expr<T> &expr2;
 
-    return sum;
-  }
-};
+    std::vector<int> computed_sizes;
+
+    barray_expr_mul(const struct barray_expr<T> &expr1,
+                    const struct barray_expr<T> &expr2)
+        : expr1(expr1), expr2(expr2)
+    {
+      computed_sizes =
+          match_expr_sizes(expr1.get_expr_size(), expr2.get_expr_size());
+    }
+
+    const std::vector<int> get_expr_size(void) const { return computed_sizes; }
+
+    const builder::dyn_var<T>
+    get_value_at(std::vector<builder::dyn_var<int> *> indices) const
+    {
+      return expr1.get_value_at(indices) * expr2.get_value_at(indices);
+    }
+  };
+
+  // Class for cross product of two expressions
+  template <typename T>
+  struct barray_expr_cross : public barray_expr<T>
+  {
+    const struct barray_expr<T> &expr1;
+    const struct barray_expr<T> &expr2;
+
+    std::vector<int> computed_sizes;
+
+    barray_expr_cross(const struct barray_expr<T> &expr1,
+                      const struct barray_expr<T> &expr2)
+        : expr1(expr1), expr2(expr2)
+    {
+      std::vector<int> size1 = expr1.get_expr_size();
+      std::vector<int> size2 = expr2.get_expr_size();
+      assert(size1.size() == 2 && size2.size() == 2 &&
+             "Cross can only be performed on 2 dimensional arrays");
+      assert(size1[1] == size2[0] && "Dimensions should match for cross product");
+      computed_sizes.push_back(size1[0]);
+      computed_sizes.push_back(size2[1]);
+    }
+
+    const std::vector<int> get_expr_size(void) const { return computed_sizes; }
+
+    const builder::dyn_var<T>
+    get_value_at(std::vector<builder::dyn_var<int> *> indices) const
+    {
+      builder::dyn_var<T> sum = 0;
+      builder::dyn_var<int> i = 0;
+      std::vector<builder::dyn_var<int> *> indices1 = {indices[0], i.addr()};
+      std::vector<builder::dyn_var<int> *> indices2 = {i.addr(), indices[1]};
+      int len = expr1.get_expr_size()[1];
+
+      // TODO: Implement cross product operator
+
+      return sum;
+    }
+  };
 } // namespace barray
 
 #endif
--- samples/sample1.cpp
+++ samples/sample1.cpp
@@ -1,11 +1,13 @@
 #include "barray.h"
 
 // BArray Program to be compiled
-void test_program(builder::dyn_var<int *> arr1, int dim1, int dim2, int dim3) {
-
+void test_program(builder::dyn_var<int *> arr1, int dim1, int dim2, int dim3)
+{
+  barray::current_device = barray::DEVICE_HOST;
   // Declare 3 arrays, with 3 dimensions
   // The first arrays used a buffer passed in
-  barray::barray<int> y(arr1, {dim1, dim2, dim3});
+  barray::barray<int>
+      y(arr1, {dim1, dim2, dim3});
   barray::barray<int> z({dim1, dim2, dim3});
   barray::barray<int> w({dim1, dim2, dim3});
 
@@ -13,14 +15,18 @@ void test_program(builder::dyn_var<int *> arr1, int dim1, int dim2, int dim3) {
   z = 0;
   // Initialize W to all ones
   w = 1;
-
+  z.to_device();
+  w.to_device();
   // Compute Z + W and store it in Y
-  y = z + w;
+  barray::run_on_gpu([&]()
+                     { y = z + w; });
+
   z.destroy();
   w.destroy();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   barray::generate_barray_program(test_program, "test", std::cout, 32, 16, 8);
   return 0;
 }
